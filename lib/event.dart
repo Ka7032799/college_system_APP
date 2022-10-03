6d245d895841e878bca338d76c197c89742fdf36
Kareem Ahmed <100070833+Ka7032799@users.noreply.github.com>
2022-10-03 03:13:05 +0200

    1

---
+++ lib/event.dart
@@ -0,0 +1,11 @@
+// ignore: unused_import
+import 'package:flutter/foundation.dart';
+
+class Event {
+  final String title;
+  Event({required this.title});
+
+  // ignore: unnecessary_this
+  @override
+  String toString() => title;
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:college_s/event.dart';
+import 'package:flutter/material.dart';
+
+import 'calendar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "ESTech Calendar",
+      home: Calendar(),
+    );
+  }
+}
